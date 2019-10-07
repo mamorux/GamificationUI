7da2647077da006f38e52348ee388d22e4b16cf2
Pecak Marcin-AMP062 <AMP062@motorolasolutions.com>
2019-10-07 21:10:18 +0200

    added material design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,29 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+      <mat-toolbar color="primary">
+          <mat-toolbar-row >
+              Gamification heroes
+          </mat-toolbar-row>
+      </mat-toolbar>
+      <mat-sidenav-container class="my-sidenav-container">
+          <mat-sidenav #sidenav mode="side">
+              <mat-nav-list>
+                  <a mat-list-item routerLink="/heroes">Heroes</a>
+                  <a mat-list-item routerLink="/dashboard">Dashboard</a>
+                  <a mat-list-item routerLink="/errors">Errors</a>
+              </mat-nav-list>
+          </mat-sidenav>
+          <mat-sidenav-content>
+              <button mat-icon-button (click)="sidenav.toggle()">
+                  <mat-icon>menu</mat-icon>
+              </button>
+              <!--    <app-ws-client></app-ws-client>-->
+              <router-outlet></router-outlet>
+          </mat-sidenav-content>
+      </mat-sidenav-container>
+       `,
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
