50f56139032d7ef298f21ddac73cfdf9fd891d98
Pecak Marcin-AMP062 <AMP062@motorolasolutions.com>
2019-10-07 21:30:57 +0200

    list of heroes initialized

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -2,11 +2,32 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-heroes',
-  templateUrl: './heroes.component.html',
+  template: `
+      <div fxLayout="column">
+          <div class="events">
+              <mat-toolbar color="primary">
+                  <mat-toolbar-row >
+                      Incidents
+                      <span class="example-spacer"></span>
+<!--                      <button mat-icon-button (click)="clearIncidents()">-->
+                          <mat-icon>delete</mat-icon>
+<!--                      </button>-->
+                  </mat-toolbar-row>
+              </mat-toolbar>
+
+
+              <app-incident-item
+                      *ngFor="let hero of heroes"
+                      [hero] = "hero"
+              >
+              </app-incident-item>
+          </div>
+      </div>
+  `,
   styleUrls: ['./heroes.component.scss']
 })
 export class HeroesComponent implements OnInit {
-
+  heroes: string[] = [];
   constructor() { }
 
   ngOnInit() {
