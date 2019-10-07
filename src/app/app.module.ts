f3c2d00eca79ce55a2ea909d24c578e8e678ee57
Pecak Marcin-AMP062 <AMP062@motorolasolutions.com>
2019-10-07 21:16:04 +0200

    added heroes and dashboard component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,14 @@ import {
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeroesComponent } from './heroes/heroes.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
